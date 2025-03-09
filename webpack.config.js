6e5bfa8e1105a6d39f461040d8142b5622052aaf
hoaiphuongtr <168563537+hoaiphuongtr@users.noreply.github.com>
2025-03-09 20:03:30 +0700

    Add files via upload

---
+++ webpack.config.js
@@ -0,0 +1,74 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+module.exports = (env) => {
+    const isDevelopment = process.env.NODE_ENV === 'development';
+    return {
+        mode: isDevelopment ? 'development' : 'production',
+        entry: {
+            app: path.resolve('src/index.js')
+        },
+        output: {
+            path: path.resolve(__dirname, 'dist'),
+            filename: '[name].[contenthash].js',
+            clean: true,
+            assetModuleFilename: '[file]'
+        },
+        devtool: isDevelopment ? 'source-map' : false,
+        module: {
+            rules: [
+                {
+                    test: /\.s[ac]ss|css$/,
+                    use: [
+                        MiniCssExtractPlugin.loader,
+                        'css-loader',
+                        'sass-loader'
+                    ]
+                },
+                {
+                    test: /\.js$/,
+                    exclude: /node_modules/,
+                    use: {
+                        loader: 'babel-loader',
+                        options: {
+                            presets: [
+                                [
+                                    '@babel/preset-env',
+                                    {
+                                        debug: true,
+                                        useBuiltIns: 'entry', //use 'entry' must manually import, use 'usage' will automatically import
+                                        corejs: '3.41.0'
+                                    }
+                                ]
+                            ]
+                        }
+                    }
+                },
+                {
+                    test: /\.(png|svg|jpg|jpeg|gif|pdf)$/i,
+                    type: 'asset/resource'
+                }
+            ]
+        },
+        plugins: [
+            new MiniCssExtractPlugin({
+                filename: '[name].[contenthash].css'
+            }),
+            new HtmlWebpackPlugin({
+                title: 'Webpack App',
+                filename: 'index.html',
+                template: './src/template.html'
+            })
+        ],
+        devServer: {
+            static: {
+                directory: 'dist' // Đường dẫn tương đối đến với thư mục chứa index.html
+            },
+            port: 3002, // Port thay cho port mặc định (8080)
+            open: true, // Mở trang webpack khi chạy terminal
+            hot: true, // Bật tính năng reload nhanh Hot Module Replacement
+            compress: true, // Bật Gzip cho các tài nguyên
+            historyApiFallback: true // Set true nếu bạn dùng cho các SPA và sử dụng History API của HTML5
+        }
+    };
+};
